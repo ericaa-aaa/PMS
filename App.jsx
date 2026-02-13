1417895d9c1ec3e46343aebf39df8c076680449b
ericaa-aaa <22-09044@g.batstate-u.edu.ph>
2026-02-13 17:21:13 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,18 +4,23 @@ import './App.css'
 
 function App() {
   return (
-    <main style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+    <main style={{ display: "flex", height: "100vh", width: "100%", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
       <div style={{ display: "flex",flexDirection: "row", alignItems:"center" , justifyContent: "center"}}>
         <img src={IntegImage} alt="Logo" className="left-integ" style={{ maxHeight :"100rem", height: "800px", float: "left"}} />
       </div>
-      <div className='right-text'>
+      <div className='right-text' style={{ flex: "1", display: "flex", flexDirection: "column", justifyContent: "center"}}>
         <div className='fontdesign1'>
-          <h1 style={{ textAlign: "justify", fontWeight: "900px"}}> Integrity</h1>
+          <h2 style={{ fontSize: "1.5rem", marginBottom: "0"}}> Integrity</h2>
         </div>
         <div className='fontdesign2'>
-          <h1 style={{ textAlign: "justify" }}> PMS</h1>
+          <h1 style={{ fontSize: "4rem", fontWeight: "bold", marginTop: "0" }}> PMS</h1>
         </div>
-      </div>
+        </div>
+        <div>
+        <input className='login' placeholder="Username" style={{ }}/>
+        <input className='password' placeholder='Password' style={{}}/>
+  
+        </div>
     </main>
   )
 }
